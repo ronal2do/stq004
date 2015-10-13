f520649b0a15e5ea8283b1f0eb70e4d888d3be98
Sotaque <sotaque@SOTMAC07.local>
2015-10-13 12:37:34 -0300

    panel

--- config/app.php
+++ config/app.php
@@ -145,6 +145,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+        * Painel
+        */
+
+        Serverfireteam\Panel\PanelServiceProvider::class,
+
     ],
 
     /*
