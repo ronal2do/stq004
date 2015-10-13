c116b4d15f9535005495a5c6e7a28ea622a11d39
Sotaque <sotaque@SOTMAC07.local>
2015-10-13 12:52:15 -0300

    Painel controllers

---
+++ app/admins.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class admins extends Model {
+
+    protected $table = 'admins';
+
+}
---
+++ app/markers.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class markers extends Model {
+
+    protected $table = 'markers';
+
+}
