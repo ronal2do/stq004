84581848945cda744f0bb00f814d7709f8f70fcd
Sotaque <sotaque@SOTMAC07.local>
2015-10-27 17:36:30 -0200

    mail

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'stqsocial@gmail.com', 'name' => 'Marília sem Dengue'],
 
     /*
     |--------------------------------------------------------------------------
