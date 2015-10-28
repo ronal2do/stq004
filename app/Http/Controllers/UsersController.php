6b1299f20c45c1107699925106bb803a245b09d1
Sotaque <sotaque@SOTMAC07.local>
2015-10-28 10:01:17 -0200

    user mail

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,49 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use \Serverfireteam\Panel\CrudController;
+
+use Illuminate\Http\Request;
+
+class UsersController extends CrudController{
+
+    public function all($entity){
+        parent::all($entity); 
+
+			$this->filter = \DataFilter::source(new \App\User);
+			$this->filter->add('name', 'Name', 'text');
+			$this->filter->submit('search');
+			$this->filter->reset('reset');
+			$this->filter->build();
+
+			$this->grid = \DataGrid::source($this->filter);
+			$this->grid->add('name', 'Name');
+			$this->grid->add('code', 'Code');
+			$this->addStylesToGrid();
+
+    
+                 
+        return $this->returnView();
+    }
+    
+    public function  edit($entity){
+        
+        parent::edit($entity);
+
+			$this->edit = \DataEdit::source(new \App\User());
+
+			$this->edit->label('Edit Category');
+
+			$this->edit->add('name', 'Name', 'text');
+		
+			$this->edit->add('code', 'Code', 'text')->rule('required');
+
+
+      
+       
+        return $this->returnEditView();
+    }    
+}
