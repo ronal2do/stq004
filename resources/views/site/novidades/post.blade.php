27dd0156644829de950283f0b6e5afbb9898255d
Sotaque <sotaque@SOTMAC07.local>
2015-10-30 19:57:25 -0200

    modificacoes 3

--- resources/views/site/novidades/post.blade.php
+++ resources/views/site/novidades/post.blade.php
@@ -14,7 +14,7 @@
               
                 <h1 class="post-title">{{$post->name}}</h1>
                 <div class="meta"><span class="date">{{$post->updated_at}}</span></div>
-                <figure class="main"><img src="/uploads/images/{{$post->image}}" alt="" /></figure>
+                <figure class="main"><img src="/uploads/images/{{$post->image}}" alt="" style="width: 100%;"/></figure>
                 <div class="post-content text-left">
                  <p>{!! $post->description !!}</p>
                   
