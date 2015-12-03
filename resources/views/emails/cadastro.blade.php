af1e0e2ce44ccd624f70a490d48b1bbfe6f3016e
Your NameRonaldo <ronal2do@gmail.com>
2015-12-03 19:41:06 -0200

    email 3

--- resources/views/emails/cadastro.blade.php
+++ resources/views/emails/cadastro.blade.php
@@ -1,6 +1,7 @@
 <h2>Oi</h2>
 
 <div>
-	<p>Bem vindo(a) {{$email}}</p>
+	<p>Bem vindo(a)</p>
+	<p>Para concluir este cadastro acesse o link abaixo</p>
 	<a href="http://mariliasemdengue.com.br/como-participar">aqui.</a>
 </div>
