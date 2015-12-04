f2e778d2dcf76f82fb95f3c4809415db5dc97a4c
Your NameRonaldo <ronal2do@gmail.com>
2015-12-04 14:26:53 -0200

    msg confirm ok

--- resources/views/emails/cadastro.blade.php
+++ resources/views/emails/cadastro.blade.php
@@ -1,7 +1,11 @@
 <h2>Oi</h2>
 
 <div>
-	<p>Bem vindo(a)</p>
-	<p>Para concluir este cadastro acesse o link abaixo</p>
-	<a href="http://mariliasemdengue.com.br/como-participar">aqui.</a>
+	<p>Obrigada(o) por se inscrever</p>
+	<p>Você está quase lá! Clique <a href="http://mariliasemdengue.com.br/como-participar">aqui</a> para concluir seu cadastro.</p>
+	
 </div>
+
+
+
+
--- resources/views/emails/voluntario.blade.php
+++ resources/views/emails/voluntario.blade.php
@@ -1,7 +1,9 @@
-<h1>
-	<p>
-		Grato!
-	</p>
+<h3>Obrigada(o) por se cadastrar
+</h3>
+<p>Agora você faz parte do time de voluntários do Marília sem dengue. Em breve entraremos em 
 
-	<p>Horário das reuniões {{$email}}</p>
-</h1>
+contato com você para passar as primeiras orientações. </p>
+
+<p>Para mais informações sobre a campanha, dicas e notícias, acesse: </p>
+
+<a href="www.mariliasemdengue.com.br">www.mariliasemdengue.com.br</a>
